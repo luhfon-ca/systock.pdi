75ad86ee8c4513b0e02acdbca6a18dd0dec2e675
luhfon-ca <luhfon.ca@gmail.com>
2024-07-03 12:25:49 -0400

    restore do backup_a.dm e backup_b.dmp

--- backup_b.sql
+++ backup_b.sql
@@ -16,6 +16,52 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: consumos_a; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.consumos_a (
+    ano integer NOT NULL,
+    mes integer NOT NULL,
+    status character varying(10) NOT NULL,
+    valor_total numeric(12,4) DEFAULT 0 NOT NULL
+);
+
+
+ALTER TABLE public.consumos_a OWNER TO postgres;
+
+--
+-- Data for Name: consumos_a; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.consumos_a (ano, mes, status, valor_total) FROM stdin;
+2024	1	VENDA	700000.0000
+2024	1	DEVOLUCAO	70000.0000
+2024	2	VENDA	500000.0000
+2024	2	DEVOLUCAO	50000.0000
+2024	3	VENDA	700000.0000
+2024	3	DEVOLUCAO	70000.0000
+2024	4	VENDA	500000.0000
+2024	4	DEVOLUCAO	50000.0000
+2024	5	VENDA	700000.0000
+2024	5	DEVOLUCAO	70000.0000
+2024	6	VENDA	500000.0000
+2024	6	DEVOLUCAO	50000.0000
+\.
+
+
+--
+-- Name: consumos_a pk_consumo_a; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.consumos_a
+    ADD CONSTRAINT pk_consumo_a PRIMARY KEY (ano, mes, status);
+
+
 --
 -- PostgreSQL database dump complete
 --
